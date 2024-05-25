4a19ab3f90e2d45895e55cec4174cc0624e6e9f1
Khaled Saleh <k.salehmah@gmail.com>
2024-05-26 02:57:39 +1000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import camelot
+import base64
+import uuid
+import pdfplumber
+import pandas as pd
+
+
+
+
+def main():
+    """Streamlit app testing camelot.py"""
+
+    st.write("# PDF2Excel Parser!")
+
+    uploaded_file = st.file_uploader(label="Upload PDF with tables", type="pdf")
+    if uploaded_file is not None:
+        with pdfplumber.open(uploaded_file) as file:
+
+            all_pages = file.pages
+            tbl = all_pages[0].extract_table()
+            original_df = pd.DataFrame(tbl)
+            st.dataframe(original_df)  # Same as st.write(df)
+
+    else:
+        st.write("Upload a PDF file with some tables in it.")
+
+
+if __name__ == "__main__":
+    main()
